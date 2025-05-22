a04e3094a76fb8b04c6db5f948e45913449277d2
Otherboi <clement@cmoussy.eu>
2025-05-22 16:55:08 +0200

    Added the first few functions for 24b

--- bmp24.c
+++ bmp24.c
@@ -1,1 +1,29 @@
 #include "bmp24.h"
+
+t_pixel ** bmp24_allocateDataPixels(int width, int height) {
+    t_pixel ** pixels = malloc(height * width * sizeof(t_pixel *));
+    return pixels;
+}
+
+void bmp24_freeDataPixels (t_pixel ** pixels, int height) {
+    free(pixels);
+}
+
+t_bmp24 * bmp24_allocate(int width, int height, int colorDepth) {
+    t_pixel ** pixels = bmp24_allocateDataPixels(width, height);
+    t_bmp24 * bmp24 = (t_bmp24 *) malloc(sizeof(t_bmp24));
+    bmp24->width = width;
+    bmp24->height = height;
+    bmp24->colorDepth = colorDepth;
+    bmp24->data = pixels;
+    if (!(bmp24->data)) {
+        free(bmp24);
+        return NULL;
+    }
+    return bmp24;
+}
+
+void bmp24_free(t_bmp24 * bmp24) {
+    free(bmp24->data);
+    free(bmp24);
+}
