fd5aa9e0788de179bd9a4d0b74cebc368210fab0
Meta <rafy.vec@gmail.com>
2025-05-24 23:17:32 +0200

    Revert "Delete cmake-build-debug directory"
    This reverts commit 0c2dc8d8fa27cd4aba52e6620cc27e19b63154cb.

---
+++ cmake-build-debug/CMakeFiles/image_processing_veclin_moussy_int1.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/rafyv/CLionProjects/image-processing-veclin-moussy-int1/bmp24.c" "CMakeFiles/image_processing_veclin_moussy_int1.dir/bmp24.c.obj" "gcc" "CMakeFiles/image_processing_veclin_moussy_int1.dir/bmp24.c.obj.d"
+  "C:/Users/rafyv/CLionProjects/image-processing-veclin-moussy-int1/bmp8.c" "CMakeFiles/image_processing_veclin_moussy_int1.dir/bmp8.c.obj" "gcc" "CMakeFiles/image_processing_veclin_moussy_int1.dir/bmp8.c.obj.d"
+  "C:/Users/rafyv/CLionProjects/image-processing-veclin-moussy-int1/main.c" "CMakeFiles/image_processing_veclin_moussy_int1.dir/main.c.obj" "gcc" "CMakeFiles/image_processing_veclin_moussy_int1.dir/main.c.obj.d"
+  "C:/Users/rafyv/CLionProjects/image-processing-veclin-moussy-int1/utils.c" "CMakeFiles/image_processing_veclin_moussy_int1.dir/utils.c.obj" "gcc" "CMakeFiles/image_processing_veclin_moussy_int1.dir/utils.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/image_processing_veclin_moussy_int1.dir/utils.c.obj.d
@@ -0,0 +1,21 @@
+CMakeFiles/image_processing_veclin_moussy_int1.dir/utils.c.obj: \
+ C:\Users\rafyv\CLionProjects\image-processing-veclin-moussy-int1\utils.c \
+ C:\Users\rafyv\CLionProjects\image-processing-veclin-moussy-int1\utils.h \
+ C:/Program\ Files/JetBrains/CLion\ 2025.1/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/stdint.h \
+ C:/Program\ Files/JetBrains/CLion\ 2025.1/bin/mingw/x86_64-w64-mingw32/include/stdint.h \
+ C:/Program\ Files/JetBrains/CLion\ 2025.1/bin/mingw/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/Program\ Files/JetBrains/CLion\ 2025.1/bin/mingw/x86_64-w64-mingw32/include/corecrt.h \
+ C:/Program\ Files/JetBrains/CLion\ 2025.1/bin/mingw/x86_64-w64-mingw32/include/_mingw.h \
+ C:/Program\ Files/JetBrains/CLion\ 2025.1/bin/mingw/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/Program\ Files/JetBrains/CLion\ 2025.1/bin/mingw/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/Program\ Files/JetBrains/CLion\ 2025.1/bin/mingw/x86_64-w64-mingw32/include/vadefs.h \
+ C:/Program\ Files/JetBrains/CLion\ 2025.1/bin/mingw/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/Program\ Files/JetBrains/CLion\ 2025.1/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/stddef.h \
+ C:/Program\ Files/JetBrains/CLion\ 2025.1/bin/mingw/x86_64-w64-mingw32/include/stddef.h \
+ C:/Program\ Files/JetBrains/CLion\ 2025.1/bin/mingw/x86_64-w64-mingw32/include/stdio.h \
+ C:/Program\ Files/JetBrains/CLion\ 2025.1/bin/mingw/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ C:/Program\ Files/JetBrains/CLion\ 2025.1/bin/mingw/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/Program\ Files/JetBrains/CLion\ 2025.1/bin/mingw/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/Program\ Files/JetBrains/CLion\ 2025.1/bin/mingw/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ C:/Program\ Files/JetBrains/CLion\ 2025.1/bin/mingw/x86_64-w64-mingw32/include/string.h \
+ C:/Program\ Files/JetBrains/CLion\ 2025.1/bin/mingw/x86_64-w64-mingw32/include/sec_api/string_s.h
